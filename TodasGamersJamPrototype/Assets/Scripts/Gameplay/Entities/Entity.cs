7ca22aa46e7bb583c0e16e1f4b2fd4d86c48ed9a
Francisco <fraurerod@gmail.com>
2017-08-29 20:39:23 +0200

    prueba webhook

---
+++ TodasGamersJamPrototype/Assets/Scripts/Gameplay/Entities/Entity.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    private void Awake()
+    {
+        InheritedAwake();
+    }
+    
+    public virtual void InheritedAwake()
+    {
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+        InheritedStart();
+    }
+
+    public virtual void InheritedStart()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        InheritedUpdate();
+    }
+
+    public virtual void InheritedUpdate()
+    {
+    }
+}
---
+++ TodasGamersJamPrototype/Assets/Scripts/Gameplay/Entities/Witch.cs
@@ -0,0 +1,20 @@
+﻿namespace Gameplay.Entities
+{
+    public class Witch : Entity
+    {
+        public override void InheritedAwake()
+        {
+            base.InheritedAwake();
+        }
+
+        public override void InheritedStart()
+        {
+            base.InheritedStart();
+        }
+
+        public override void InheritedUpdate()
+        {
+            base.InheritedUpdate();
+        }
+    }
+}
